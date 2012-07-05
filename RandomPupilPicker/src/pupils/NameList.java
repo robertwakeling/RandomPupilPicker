f14578e4b8b339f1b65a8d516934f356dc9cc26d
Leon Ormes <leonormes@gmail.com>
2012-07-05 21:22:04 +0100

    Added all names to ArrayList

--- RandomPupilPicker/src/pupils/NameList.java
+++ RandomPupilPicker/src/pupils/NameList.java
@@ -1,31 +1,51 @@
 package pupils;
 
 
-// Create arraylist with all pupils names in
 import java.util.*; 
+
+//Create arraylist with all pupils names in
+
 class NameList { 
+	
+	
 	public static void main(String args[]) { 
+		
 // create an array list 
 			ArrayList pupil = new ArrayList(); 
-			System.out.println("Initial size of al: " + 
-					pupil.size()); 
+
 // add elements to the array list 
-			pupil.add("C"); 
-			pupil.add("A"); 
-			pupil.add("E"); 
-			pupil.add("B"); 
-			pupil.add("D"); 
-			pupil.add("F"); 
-			pupil.add(1, "A2"); 
-			System.out.println("Size of list after additions: " + 
-					pupil.size()); 
+			
+			pupil.add("Bethany"); 
+			pupil.add("Mahalah"); 
+			pupil.add("Paige"); 
+			pupil.add("Finley"); 
+			pupil.add("Skye"); 
+			pupil.add("Tayla"); 
+			pupil.add("Archie");
+			pupil.add("Harley");
+			pupil.add("Declan");
+			pupil.add("Sam");
+			pupil.add("Joseph");
+			pupil.add("Molly");
+			pupil.add("Lillie");
+			pupil.add("Vera-Lee");
+			pupil.add("Mitchell");
+			pupil.add("Joshua");
+			pupil.add("Millissa");
+			pupil.add("Vinnie");
+			pupil.add("Henry");
+			pupil.add("Elijah");
+			pupil.add("Toby");
+			pupil.add("Isabella");
+			pupil.add("Lillyann");
+			pupil.add("Aoife");
+			pupil.add("Billy");
+			pupil.add("Junior");
+			
+
 // display the array list 
-			System.out.println("Contents of list: " + pupil); 
-// Remove elements from the array list 
-			pupil.remove("F"); 
-			pupil.remove(2); 
-			System.out.println("Size of list after deletions: " + 
-					pupil.size()); 
-			System.out.println("Contents of list: " + pupil); 
+			System.out.println("Pupils left to choose from: \n" + pupil); 
+			
+
 	} 
 }
