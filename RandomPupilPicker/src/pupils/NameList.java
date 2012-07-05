e64e9ae8d6288e4106bbe784a0c0f57b0abcc9ad
Leon Ormes <leonormes@gmail.com>
2012-07-05 20:55:34 +0100

    Arraylist set up from a copy of the net

--- RandomPupilPicker/src/pupils/NameList.java
+++ RandomPupilPicker/src/pupils/NameList.java
@@ -1,13 +1,31 @@
 package pupils;
 
-import java.util.ArrayList;
-import java.util.Random;
 
-class NameList {
-	
-	public static void picker() {
-		
-		ArrayList learners = new ArrayList();
-		
-	}
+// Create arraylist with all pupils names in
+import java.util.*; 
+class NameList { 
+	public static void main(String args[]) { 
+// create an array list 
+			ArrayList pupil = new ArrayList(); 
+			System.out.println("Initial size of al: " + 
+					pupil.size()); 
+// add elements to the array list 
+			pupil.add("C"); 
+			pupil.add("A"); 
+			pupil.add("E"); 
+			pupil.add("B"); 
+			pupil.add("D"); 
+			pupil.add("F"); 
+			pupil.add(1, "A2"); 
+			System.out.println("Size of list after additions: " + 
+					pupil.size()); 
+// display the array list 
+			System.out.println("Contents of list: " + pupil); 
+// Remove elements from the array list 
+			pupil.remove("F"); 
+			pupil.remove(2); 
+			System.out.println("Size of list after deletions: " + 
+					pupil.size()); 
+			System.out.println("Contents of list: " + pupil); 
+	} 
 }
